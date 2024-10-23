6c15108c2b9005f723f66b6c1f195c3e17f0de49
Alice Lazzeri <alice.lazzeri@hotmail.it>
2024-10-23 11:56:09 +0200

    added tree card

---
+++ frontend/src/components/TreeCard.tsx
@@ -0,0 +1,61 @@
+import React from "react";
+import { RiToolsFill } from "react-icons/ri";
+import { IoCloseSharp, IoNavigateCircleOutline } from "react-icons/io5";
+import placeholder from "../../public/placeholder.png";
+
+interface TreeCardProps {
+  tipo: string;
+  nome: string;
+  stato: string;
+  ultimaSegnalazione: string;
+  onClose: () => void;
+}
+
+const TreeCard: React.FC<TreeCardProps> = ({ tipo, nome, stato, ultimaSegnalazione, onClose }) => {
+  return (
+    <div className="fixed inset-0 bg-black bg-opacity-50 z-[999] flex justify-center items-center">
+      <div className="bg-white px-6 pt-5 pb-9 rounded-lg shadow-2xl w-150 relative flex flex-col items-center">
+        <button onClick={onClose} className="absolute top-3 text-[#878585] right-3 text-2xl">
+          <IoCloseSharp size={20} />
+        </button>
+
+        <h2 className="text-xl font-bold text-[#334D42] mb-4">{nome}</h2>
+        <div className="flex items-center gap-3">
+          <img
+            src={placeholder}
+            alt={nome}
+            width={150}
+            height={250}
+            className="object-cover rounded-lg opacity-75"
+            style={{ boxShadow: "4px 4px 4px 0px #00000040" }}
+          />
+
+          <div className="text-left w-full px-4">
+            <p className="text-[#878585] font-light text-sm mb-1">Type</p>
+            <p className="font-normal mb-3">{tipo}</p>
+            <p className="text-[#878585] font-light text-sm mb-1">State</p>
+            <p className="font-normal mb-3">{stato}</p>
+            <p className="text-[#878585] font-light text-sm mb-1">Last Report</p>
+            <p className="font-normal mb-3">
+              {new Date(ultimaSegnalazione).toLocaleDateString("it-IT", {
+                day: "numeric",
+                month: "long",
+                year: "numeric",
+              })}
+            </p>
+            <div className="flex justify-between gap-5 mt-4 w-full">
+              <button className="bg-[#334D42] text-[#EFE9CE] p-2 rounded-full shadow-lg">
+                <IoNavigateCircleOutline size={30} />
+              </button>
+              <button className="bg-[#CE6146] text-[#EFE9CE] p-2 rounded-full shadow-lg">
+                <RiToolsFill size={30} />
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default TreeCard;
--- frontend/src/pages/MapPage.tsx
+++ frontend/src/pages/MapPage.tsx
@@ -7,76 +7,108 @@ import { Logo } from "../components/Logo";
 import { treeMarker, userMarker } from "../components/markers";
 import SearchInput from "../components/Search";
 import { MdOutlineMyLocation } from "react-icons/md";
+import TreeCard from "../components/TreeCard";
 
 export interface Place {
-    display_name?: string;
-    lat?: number;
-    lon?: number;
+  display_name?: string;
+  lat?: number;
+  lon?: number;
+}
+
+interface TreeData {
+  ID: number;
+  Tipo: string;
+  Nome: string;
+  Latitudine: number;
+  Longitudine: number;
+  Stato: string;
+  "Ultima Segnalazione": string;
 }
 
 const FlyToUserLocation = ({ position }: { position: [number, number] | null }) => {
-    const map = useMap();
+  const map = useMap();
 
-    useEffect(() => {
-        if (position) {
-            map.flyTo(position, 13);
-        }
-    }, [position, map]);
+  useEffect(() => {
+    if (position) {
+      map.flyTo(position, 13);
+    }
+  }, [position, map]);
 
-    return null;
-}
+  return null;
+};
 
 const MapPage = () => {
-    const { position } = useUserLocation();
-    const [location, setLocation] = useState<Place | null>(null)
+  const { position } = useUserLocation();
+  const [location, setLocation] = useState<Place | null>(null);
+  const [selectedTree, setSelectedTree] = useState<TreeData | null>(null);
 
-    const currentLocation: [number, number] = useMemo(() => {
-        if (location?.lat && location?.lon) {
-            return [Number(location.lat), Number(location.lon)]
-        }
+  const currentLocation: [number, number] = useMemo(() => {
+    if (location?.lat && location?.lon) {
+      return [Number(location.lat), Number(location.lon)];
+    }
 
-        if (position) {
-            return position
-        }
+    if (position) {
+      return position;
+    }
 
-        return [51.505, -0.09]
-    }, [location, position])
+    return [51.505, -0.09];
+  }, [location, position]);
 
-    return (
-        <div className="h-screen relative flex items-center">
-            <MapContainer center={currentLocation} zoom={13} zoomControl={false} className="w-full h-screen">
-                <TileLayer
-                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-                    attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-                />
-                {position && <Marker position={position} icon={userMarker()} />}
-                <FlyToUserLocation position={currentLocation} />
-                {itemsData.map((item) => {
-                    const isBadCondition = item.Stato !== "Buono";
-                    return (
-                        <Marker
-                            key={item.ID}
-                            position={[item.Latitudine, item.Longitudine]}
-                            icon={treeMarker(isBadCondition)}
-                        />
-                    );
-                })}
-            </MapContainer>
-            <div className="absolute top-0 left-0 flex justify-center items-center p-4 w-full z-[999]">
-                <SearchInput setLocation={setLocation} />
-            </div>
-            <div className="w-full fixed bottom-0 left-0 z-[999] flex justify-center items-center">
-                <button>
-                    <Logo />
-                </button>
-            </div>
-            <div className="absolute top-1/2 right-2 bg-white box-shadow p-2 rounded-lg z-[999] flex justify-center items-center">
-                <button onClick={() => setLocation(null)}>
-                    <MdOutlineMyLocation size={30} />
-                </button>
-            </div>
-        </div>
-    );
+  const handleCloseModal = () => {
+    setSelectedTree(null);
+  };
+
+  return (
+    <div className="h-screen relative flex items-center">
+      <MapContainer center={currentLocation} zoom={13} zoomControl={false} className="w-full h-screen">
+        <TileLayer
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        />
+        {position && <Marker position={position} icon={userMarker()} />}
+        <FlyToUserLocation position={currentLocation} />
+        {itemsData.map(item => {
+          const isBadCondition = item.Stato !== "Buono";
+          return (
+            <Marker
+              key={item.ID}
+              position={[item.Latitudine, item.Longitudine]}
+              icon={treeMarker(isBadCondition)}
+              eventHandlers={{
+                click: () => setSelectedTree(item),
+              }}
+            />
+          );
+        })}
+      </MapContainer>
+
+      <div className="absolute top-0 left-0 flex justify-center items-center p-4 w-full z-[999]">
+        <SearchInput setLocation={setLocation} />
+      </div>
+
+      <div className="w-full fixed bottom-0 left-0 z-[999] flex justify-center items-center">
+        <button>
+          <Logo />
+        </button>
+      </div>
+
+      <div className="absolute top-1/2 right-2 bg-white box-shadow p-2 rounded-lg z-[999] flex justify-center items-center">
+        <button onClick={() => setLocation(null)}>
+          <MdOutlineMyLocation size={30} />
+        </button>
+      </div>
+
+      {selectedTree && (
+        <TreeCard
+          tipo={selectedTree.Tipo}
+          nome={selectedTree.Nome}
+          stato={selectedTree.Stato}
+          ultimaSegnalazione={selectedTree["Ultima Segnalazione"]}
+          onClose={handleCloseModal}
+        />
+      )}
+    </div>
+  );
 };
 
 export default MapPage;
