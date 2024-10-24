3c8a11d7669a9033e12150356444a99307d953b8
DavideBri <bbrienza99@gmail.com>
2024-10-24 10:44:19 +0200

    Aggiunto Pulsanti di segnalazione
    In caso in cui il pulsante di Segnalazione viene premuto, posso
    aggiungere eventuali report piu sepicifici.

--- frontend/src/components/Modal.tsx
+++ frontend/src/components/Modal.tsx
@@ -1,133 +1,199 @@
-import { useState } from "react"
-import SearchInput from "./Search"
-import { TbLocationFilled } from "react-icons/tb"
-import { useUserLocation } from "../hooks/useUserLocation"
-import { Report } from "../types"
-import { useReports } from "../hooks/useReports"
-import { z, ZodError } from "zod"
-import { AxiosError } from "axios"
+import { useState } from "react";
+import SearchInput from "./Search";
+import { TbLocationFilled } from "react-icons/tb";
+import { useUserLocation } from "../hooks/useUserLocation";
+import { Report } from "../types";
+import { useReports } from "../hooks/useReports";
+import { z, ZodError } from "zod";
+import { AxiosError } from "axios";
 
 interface ModalProps {
-    isOpen: boolean
-    setIsOpen: (value: boolean) => void
+  isOpen: boolean;
+  setIsOpen: (value: boolean) => void;
 }
 
 const ReportSchema = z.object({
-    tipo: z.string()
-        .min(1, { message: "Type is required" }),
-    nome: z.string()
-        .min(1, { message: "Name is required" }),
-    latitudine: z.string()
-        .min(1, { message: "Place is required" }),
-    longitudine: z.string()
-        .min(1, { message: "Place is required" }),
-    stato: z.string()
-        .min(1, { message: "Status is required" }),
+  tipo: z.string().min(1, { message: "Type is required" }),
+  nome: z.string().min(1, { message: "Name is required" }),
+  latitudine: z.string().min(1, { message: "Place is required" }),
+  longitudine: z.string().min(1, { message: "Place is required" }),
+  stato: z.string().min(1, { message: "Status is required" }),
 });
 
 const Modal = ({ isOpen, setIsOpen }: ModalProps) => {
-    const [report, setReport] = useState<Report | null>(null)
-    const { position } = useUserLocation()
-    const { addReport, loading } = useReports()
-    const [error, setError] = useState<string>()
+  const [report, setReport] = useState<Report | null>(null);
+  const { position } = useUserLocation();
+  const { addReport, loading } = useReports();
+  const [error, setError] = useState<string>();
 
-    const handleChange = (key: string, value: string) => {
-        const newReport = report ? { ...report } : {}
-        setReport({ ...newReport, [key]: value } as Report)
+  const handleChange = (key: string, value: string) => {
+    const newReport = report ? { ...report } : {};
+    setReport({ ...newReport, [key]: value } as Report);
+  };
+
+  const handleChangeLocation = (newLocation: [number, number]) => {
+    const newReport = report ? { ...report } : {};
+    setReport({
+      ...newReport,
+      latitudine: newLocation[0].toString(),
+      longitudine: newLocation[1].toString(),
+    } as Report);
+  };
+
+  const [stato, setStato] = useState("");
+  const handleButtonClick = (valore: string) => {
+    setStato(valore);
+    handleChange("stato", valore); // Chiamata a handleChange con il nuovo valore
+  };
+
+  const handleSubmit = () => {
+    try {
+      ReportSchema.parse(report);
+      addReport({
+        ...report,
+        ultima_segnalazione: new Date().toISOString().split("T")[0],
+      } as Report);
+      setReport(null);
+      setError("");
+      setIsOpen(false);
+    } catch (err) {
+      if (err instanceof ZodError) {
+        setError(err.errors[0].message);
+      } else if (err instanceof AxiosError || err instanceof Error) {
+        setError(err.message);
+      } else {
+        setError("Ops, something went wrong!");
+      }
     }
+  };
 
-    const handleChangeLocation = (newLocation: [number, number]) => {
-        const newReport = report ? { ...report } : {}
-        setReport({
-            ...newReport,
-            latitudine: newLocation[0].toString(),
-            longitudine: newLocation[1].toString()
-        } as Report)
-    }
-
-    const handleSubmit = () => {
-        try {
-            ReportSchema.parse(report)
-            addReport({ ...report, ultima_segnalazione: new Date().toISOString().split('T')[0] } as Report)
-            setReport(null)
-            setError("")
-            setIsOpen(false)
-        } catch (err) {
-            if (err instanceof ZodError) {
-                setError(err.errors[0].message)
-            } else if (err instanceof AxiosError || err instanceof Error) {
-                setError(err.message);
-            } else {
-                setError("Ops, something went wrong!")
-            }
-        }
-    }
-
-
-    return (
-        <div id="slideover-container" className={`w-full h-full fixed inset-0 flex justify-center items-center ${isOpen ? '' : 'invisible'} z-[500]`}>
-            <div
-                onClick={() => {
-                    setReport(null)
-                    setIsOpen(false)
-                }}
-                id="slideover-bg"
-                className={`w-full h-full duration-500 ease-out transition-all inset-0 absolute bg-gray-900 ${isOpen ? 'opacity-50' : 'opacity-0'}`}
-            />
-            <div id="slideover" className={`w-full rounded-t-[50px] bg-white h-full absolute duration-300 ease-out transition-all p-4 ${isOpen ? 'top-20' : 'top-full'}`}>
-                <div className="w-full h-[90%] flex flex-col justify-start items-center">
-                    <div className="w-1/2 bg-gray-200 rounded-full h-[7px]" />
-                    <p className="font-semibold text-xl mt-2">Report</p>
-                    <div className="flex flex-col w-full h-full p-2 gap-4">
-                        <div className="flex flex-col justify-center gap-1">
-                            <span className="font-semibold">Type</span>
-                            <input type="text" placeholder="Add a type..." className="border border-black outline-none p-2" onChange={(e) => handleChange("tipo", e.currentTarget.value)} />
-                        </div>
-                        <div className="flex flex-col justify-center gap-1">
-                            <span className="font-semibold">Name</span>
-                            <input type="text" placeholder="Add a name..." className="border border-black outline-none p-2" onChange={(e) => handleChange("nome", e.currentTarget.value)} />
-                        </div>
-                        <div className="flex flex-col justify-center gap-1">
-                            <span className="font-semibold">Location</span>
-                            <div className="relative">
-                                <SearchInput
-                                    setLocation={(place) => {
-                                        handleChangeLocation([place?.lat, place?.lon] as [number, number])
-                                    }}
-                                    className="border border-black outline-none p-2 w-full"
-                                />
-                                <button className="absolute top-[3px] right-1 p-2" onClick={() => {
-                                    if (position) {
-                                        handleChangeLocation(position)
-                                    }
-                                }}>
-                                    <TbLocationFilled size={20} />
-                                </button>
-                            </div>
-                        </div>
-                        <div className="flex flex-col justify-center gap-1">
-                            <span className="font-semibold">Status</span>
-                            <input type="text" placeholder="Add a status..." className="border border-black outline-none p-2" onChange={(e) => handleChange("stato", e.currentTarget.value)} />
-                        </div>
-                        <div className="flex flex-col justify-center gap-1 h-full">
-                            <span className="font-semibold">Image</span>
-                            <div className="border-2 border-dashed border-gray-400 bg-gray-100 w-full h-full flex justify-center items-center">
-                                <p className="text-center text-gray-400">Add an image <br />(Optional)</p>
-                            </div>
-                        </div>
-                        {error && <p className="font-semibold text-red-500">{error}</p>}
-                        <button
-                            disabled={loading}
-                            className={`w-full p-4 ${loading ? 'bg-[#334D42]/75 text-[#EFE9CE]/75' : 'bg-[#334D42] text-[#EFE9CE]'} bg-[#334D42] text-[#EFE9CE] font-semibold text-lg rounded-[4px]`}
-                            onClick={handleSubmit}
-                        >
-                            Submit
-                        </button>
-                    </div>
-                </div>
+  return (
+    <div
+      id="slideover-container"
+      className={`w-full h-full fixed inset-0 flex justify-center items-center ${isOpen ? "" : "invisible"} z-[500]`}
+    >
+      <div
+        onClick={() => {
+          setReport(null);
+          setIsOpen(false);
+        }}
+        id="slideover-bg"
+        className={`w-full h-full duration-500 ease-out transition-all inset-0 absolute bg-gray-900 ${isOpen ? "opacity-50" : "opacity-0"}`}
+      />
+      <div
+        id="slideover"
+        className={`w-full rounded-t-[50px] bg-white h-full absolute duration-300 ease-out transition-all p-4 ${isOpen ? "top-20" : "top-full"}`}
+      >
+        <div className="w-full h-[90%] flex flex-col justify-start items-center">
+          <div className="w-1/2 bg-gray-200 rounded-full h-[7px]" />
+          <p className="font-semibold text-xl mt-2">Report</p>
+          <div className="flex flex-col w-full h-full p-2 gap-4">
+            <div className="flex flex-col justify-center gap-1">
+              <span className="font-semibold">Type</span>
+              <input
+                type="text"
+                placeholder="Add a type..."
+                className="border border-black outline-none p-2"
+                onChange={(e) => handleChange("tipo", e.currentTarget.value)}
+              />
             </div>
-        </div>
-    )
-}
+            <div className="flex flex-col justify-center gap-1">
+              <span className="font-semibold">Name</span>
+              <input
+                type="text"
+                placeholder="Add a name..."
+                className="border border-black outline-none p-2"
+                onChange={(e) => handleChange("nome", e.currentTarget.value)}
+              />
+            </div>
+            <div className="flex flex-col justify-center gap-1">
+              <span className="font-semibold">Location</span>
+              <div className="relative">
+                <SearchInput
+                  setLocation={(place) => {
+                    handleChangeLocation([place?.lat, place?.lon] as [
+                      number,
+                      number,
+                    ]);
+                  }}
+                  className="border border-black outline-none p-2 w-full"
+                />
+                <button
+                  className="absolute top-[3px] right-1 p-2"
+                  onClick={() => {
+                    if (position) {
+                      handleChangeLocation(position);
+                    }
+                  }}
+                >
+                  <TbLocationFilled size={20} />
+                </button>
+              </div>
+            </div>
+            <div className="flex flex-col justify-center gap-1">
+              <span className="font-semibold">Status</span>
+              {/* <input type="text" placeholder="Add a status..." className="border border-black outline-none p-2" onChange={(e) => handleChange("stato", e.currentTarget.value)} /> */}
 
-export default Modal
+              <button
+                onClick={() => handleButtonClick("Buono")}
+                className={`px-4 py-2 border ${
+                  stato === "Buono"
+                    ? "bg-green-500 text-white"
+                    : "bg-white text-black border-black"
+                }`}
+              >
+                In buono stato
+              </button>
+
+              <button
+                onClick={() => handleButtonClick("Malato")}
+                className={`px-4 py-2 border ${
+                  stato === "Malato"
+                    ? "bg-red-500 text-white"
+                    : "bg-white text-black border-black"
+                }`}
+              >
+                Seganalzione
+              </button>
+              {/* Input per segnalare eventuali problemi, appare solo se "Malato" è selezionato */}
+              {stato === "Malato" && (
+                <div className="flex flex-col space-y-2">
+                  <label htmlFor="problemi" className="text-gray-700">
+                    Descrivi eventuali problemi:
+                  </label>
+                  <input
+                    type="text"
+                    placeholder="Rami rotti, Malato, ..."
+                    className="border border-black outline-none p-2"
+                    onChange={(e) =>
+                      handleChange("stato", e.currentTarget.value)
+                    }
+                  />
+                </div>
+              )}
+            </div>
+            <div className="flex flex-col justify-center gap-1 h-full">
+              <span className="font-semibold">Image</span>
+              <div className="border-2 border-dashed border-gray-400 bg-gray-100 w-full h-full flex justify-center items-center">
+                <p className="text-center text-gray-400">
+                  Add an image <br />
+                  (Optional)
+                </p>
+              </div>
+            </div>
+            {error && <p className="font-semibold text-red-500">{error}</p>}
+            <button
+              disabled={loading}
+              className={`w-full p-4 ${loading ? "bg-[#334D42]/75 text-[#EFE9CE]/75" : "bg-[#334D42] text-[#EFE9CE]"} bg-[#334D42] text-[#EFE9CE] font-semibold text-lg rounded-[4px]`}
+              onClick={handleSubmit}
+            >
+              Submit
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Modal;
